24f4df016914a0921f58779759cb0671723844be
Gabriel Albuquerque <biel123albuquerque@hotmail.com>
2023-08-04 00:50:30 -0400

    Inicializa repository de Usuários

---
+++ src/main/kotlin/auth/example/repository/UserRepository.kt
@@ -0,0 +1,10 @@
+package auth.example.repository
+
+import auth.example.models.User
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.mongodb.repository.MongoRepository
+
+interface UserRepository : MongoRepository<User, Int> {
+    fun findByEmail(email: String): User?
+}
