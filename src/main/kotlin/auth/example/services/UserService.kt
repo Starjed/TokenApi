97956e0b9f494d7e9aa63e40449b0e2e14f1ec41
Gabriel Albuquerque <biel123albuquerque@hotmail.com>
2023-08-04 00:51:36 -0400

    cria service de usuários

---
+++ src/main/kotlin/auth/example/services/UserService.kt
@@ -0,0 +1,21 @@
+package auth.example.services
+
+import auth.example.models.User
+import auth.example.repository.UserRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+
+@Service
+class UserService(
+    @Autowired
+    private val userRepository: UserRepository
+) {
+
+    fun save(user: User): User {
+        return this.userRepository.save(user)
+    }
+
+    fun findByEmail(email: String): User? {
+        return this.userRepository.findByEmail(email)
+    }
+}
